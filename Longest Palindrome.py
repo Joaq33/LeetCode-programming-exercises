0e389c56694c371aa0b59f1c55f0896187f1e88b
Joaq33 <joakoillia@gmail.com>
2020-08-15 00:25:38 -0300

    Initial commit

---
+++ Longest Palindrome.py
@@ -0,0 +1,10 @@
+import collections
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        ans = 0
+        counter = collections.Counter(s)
+        for quantity in counter.values():
+            ans += quantity // 2 * 2
+        return ans + (ans < len(s))
