3a6b4bd72d5c67aea9e89c489bce004fc8c0f51f
Joaq33 <joakoillia@gmail.com>
2020-08-17 16:56:21 -0300

    Initial commit

--- Best Time to Buy and Sell Stock III.py
+++ Best Time to Buy and Sell Stock III.py
@@ -1,29 +1,34 @@
 class Solution:
     def maxProfit(self, prices) -> int:
-        # ans = []
 
-        def rec(newprices, segundo):
-            if len(newprices) < 2:
+        def rec(new_prices, second):
+            if len(new_prices) < 2:
                 return 0
-            posibles=[0]
-            for index, price in enumerate(newprices[1:]):
-                if price<newprices[0]:
-                    temp = newprices[0] - price
-                    if segundo==False:
-                        temp2 = rec(newprices[index + 1:], True)
-                        temp+=temp2
-                    posibles.append(temp)
-            maximo=max(posibles)
-            return maximo
+            posibles = [0]
+            for index, price in enumerate(new_prices[1:]):
+                if new_prices[0] >= price:
+                    continue
+                temp = price - new_prices[0]
 
-        temp=[0]
-        for item in range(len(prices)):
-            temp+=[rec(prices[item:], False)]
+                if second == False:
+                    if index + 2 == len(new_prices):
+                        posibles.append(temp)
+                        continue
+                    for i in range(index + 2, len(new_prices) - 1):
+                        temp2 = rec(new_prices[i:], True)
+                        posibles.append(temp+temp2)
+                posibles.append(temp)
+            return max(posibles)
 
-        print("temp",temp,max(temp))
-        print(prices)
+        ans = [0]
+        for item in range(len(prices) - 1):
+            ans += [rec(prices[item:], False)]
+        return max(ans)
 
 
 coso = Solution()
 prices = [3, 3, 5, 0, 0, 3, 1, 4]
+prices=[1,2,3,4,5]
+
+# prices=[1,2,3,4,5]
 print(coso.maxProfit(prices))
