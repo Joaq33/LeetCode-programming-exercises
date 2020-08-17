941721bac155c82245b2eae1c34c31cf96bc4b46
Joaq33 <joakoillia@gmail.com>
2020-08-17 15:13:35 -0300

    Initial commit

--- Best Time to Buy and Sell Stock III.py
+++ Best Time to Buy and Sell Stock III.py
@@ -1,30 +1,27 @@
 class Solution:
     def maxProfit(self, prices) -> int:
-        ans = []
+        # ans = []
 
         def rec(newprices, segundo):
-            nonlocal ans
-            posibles=[0]
             if len(newprices) < 2:
                 return 0
+            posibles=[0]
             for index, price in enumerate(newprices[1:]):
                 if price<newprices[0]:
-                    #saber si el precio es mayor q el q siga
                     temp = newprices[0] - price
-                    temp2 = rec(newprices[index + 1:], True)
-                    temp+=temp2
+                    if segundo==False:
+                        temp2 = rec(newprices[index + 1:], True)
+                        temp+=temp2
                     posibles.append(temp)
             maximo=max(posibles)
-            if maximo<0:
-                maximo=0
             return maximo
 
-        temp=[]
+        temp=[0]
         for item in range(len(prices)):
             temp+=[rec(prices[item:], False)]
 
-        print("temp",temp)
-        print(prices, ans)
+        print("temp",temp,max(temp))
+        print(prices)
 
 
 coso = Solution()
