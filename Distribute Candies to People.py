f5be2afaebd90793410c6f04ad2187c9c7ac27c9
Joaq33 <joakoillia@gmail.com>
2020-08-17 15:43:38 -0300

    Initial commit

---
+++ Distribute Candies to People.py
@@ -0,0 +1,26 @@
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int):
+        if num_people==0:
+            return []
+        ans=[0]*num_people
+        distribution=1
+        person=0
+        while candies>0:
+            if candies<distribution:
+                ans[person]+=candies
+                break
+            ans[person]+=distribution
+            candies-=distribution
+            distribution+=1
+            person+=1
+            if person>num_people-1:
+                person=0
+        return ans
+
+coso = Solution()
+param1 = 7
+param2 = 4
+
+param1 = 10
+param2 = 3
+print(coso.distributeCandies(param1, param2))
