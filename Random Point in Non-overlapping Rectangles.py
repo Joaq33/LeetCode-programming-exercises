813ec7b1a5e40bf0576ab58be97a057abaabaac8
Joaq33 <joakoillia@gmail.com>
2020-08-28 02:05:14 -0300

    Done

--- Random Point in Non-overlapping Rectangles.py
+++ Random Point in Non-overlapping Rectangles.py
@@ -1,36 +1,20 @@
-# Done but not returning
+# Done
 
 import random
 
 
 class Solution:
-
     def __init__(self, rects):
-        self.probabilities = 0
-        self.list_prob = []
-        for rectangle in rects[0][0]:
-            print(rectangle)
-            ancho = rectangle[2] - rectangle[0]
-            if ancho == 0:
-                ancho = 1
-            alto = rectangle[3] - rectangle[1]
-            if alto == 0:
-                alto = 1
-            self.list_prob.append(ancho * alto)
-            self.probabilities += ancho * alto
+        self.weights = [(x2 - x1 + 1) * (y2 - y1 + 1) for x1, y1, x2, y2 in rects]
+        self.rects = rects
 
     def pick(self):
-
-        pick = random.randrange(self.probabilities) + 1
-        whichrec = -1
-        while pick > 0:
-            whichrec += 1
-            pick -= self.list_prob[whichrec]
-        return [-pick % 4, (-pick) // 4]
+        x1, y1, x2, y2 = random.choices(self.rects, weights=self.weights)[0]
+        return [random.randint(x1, x2), random.randint(y1, y2)]
 
 
 # Your Solution object will be instantiated and called as such:
-rects = [[[[1, 1, 5, 5]]], [], [], []]
+rects = [[1, 1, 5, 5]]
 # rects = [[[[-2,-2,-1,-1],[1,0,3,0],[4,6,5,6],[1,0,3,0],[4,6,7,9],[1,0,3,0],[4,6,10,6]]],[],[],[],[],[]]
 obj = Solution(rects)
 param_1 = obj.pick()
