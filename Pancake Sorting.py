3cc5a15d5117292fd9a98f085f65c7f9c9d4692a
Joaq33 <joakoillia@gmail.com>
2020-08-29 21:12:26 -0300

    Done

--- Pancake Sorting.py
+++ Pancake Sorting.py
@@ -5,13 +5,14 @@ from typing import List
 class Solution:
     def pancakeSort(self, A: List[int]) -> List[int]:
         ans = []
-        for item in range(len(A), 0, -1):
-            current = A.index(len(A) - item + 1) + 1
+        length = len(A)
+        for item in range(length, 0, -1):
+            current = A.index(length - item + 1) + 1
             ans.append(current)
             A = A[current - 1::-1] + A[current:]
             ans.append(item)
             A = A[item - 1::-1] + A[item:]
-        ans.append(len(A))
+        ans.append(length)
         return ans
 
 
