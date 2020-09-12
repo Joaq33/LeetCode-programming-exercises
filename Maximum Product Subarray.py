f61d606ddd3e2e510e940bb532878f8458e50e63
Joaq33 <joakoillia@gmail.com>
2020-09-11 21:23:36 -0300

    Done (slow)

--- Maximum Product Subarray.py
+++ Maximum Product Subarray.py
@@ -1,6 +1,6 @@
 # Done (too slow)
-from typing import List
 import math
+from typing import List
 
 
 class Solution:
@@ -12,33 +12,30 @@ class Solution:
             if item == 1:
                 ans = 1
             elif item == -1:
-                    quantityofneg += 1
+                quantityofneg += 1
             else:
                 if quantityofneg == 1:
                     newlist.append(-1)
                     quantityofneg = 0
                 elif quantityofneg > 1:
-                    newlist.extend([-1,-1])
+                    newlist.extend([-1, -1])
                     quantityofneg = 0
                 newlist.append(item)
         if quantityofneg == 1:
             newlist.append(-1)
-        elif quantityofneg >1:
-            newlist.extend([-1,-1])
+        elif quantityofneg > 1:
+            newlist.extend([-1, -1])
 
         nums = newlist
         n = len(nums)
-        for largo in range(1, n+1):
-            for pos in range(n-largo+1):
-                temp = math.prod(nums[pos:pos+largo])
+        for largo in range(1, n + 1):
+            for pos in range(n - largo + 1):
+                temp = math.prod(nums[pos:pos + largo])
                 if ans < temp:
                     ans = temp
         return ans
 
 
 obj = Solution()
-nums = [2, 3, 1, -2, -1, 3, 419]
-# nums = [2, 3, 1, -2, -1,-1, 3, 419]
-nums = [0, 0, -3, 1]
 nums = [2, -1, -1, 2, 0, -3, 3]
 print(obj.maxProduct(nums))
