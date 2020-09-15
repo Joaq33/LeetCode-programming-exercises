b2f0e7feef115811f750e0c5ec7aaa076a9f27ac
Joaq33 <joakoillia@gmail.com>
2020-09-15 14:35:48 -0300

    Done

---
+++ Length of Last Word.py
@@ -0,0 +1,12 @@
+# Done
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.split()
+        if len(s) == 0:
+            return 0
+        return len(s[-1])
+
+
+obj = Solution()
+s = ""
+print(obj.lengthOfLastWord(s))
