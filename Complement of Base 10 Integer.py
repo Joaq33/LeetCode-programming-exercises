0242b1cc43ae68523ebcb61d725ee12835e2ab36
Joaq33 <joakoillia@gmail.com>
2020-10-05 19:32:08 -0300

    Done

--- Complement of Base 10 Integer.py
+++ Complement of Base 10 Integer.py
@@ -1,18 +1,5 @@
 class Solution:
     def bitwiseComplement(self, N: int) -> int:
-        if N == 0:
-            return 1
-
-        if N == 1:
-            return 0
-
-        up = 2
-        while up <= N:
-            up = up << 1
-
-        return up - 1 - N
-
-    def bitwiseComplement2(self, N: int) -> int:
         return 2 ** len("{0:b}".format(N)) - 1 ^ N
 
 
