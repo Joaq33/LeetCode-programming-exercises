d73ad7680824368f84e2179a8e7d4f30bc2b9698
Joaq33 <joakoillia@gmail.com>
2020-10-15 20:55:38 -0300

    Done

--- __Aux funcs.pyi
+++ __Aux funcs.pyi
@@ -1,6 +1,24 @@
 # These are some handy functions to enhance the programming experience
 
+def reverse(nums, start, end):
+    """
+    Reverse a list.
+    :param nums:
+    :param start:
+    :param end:
+    :return:
+    """
+    while start < end:
+        nums[start], nums[end - 1] = nums[end - 1], nums[start]
+        start += 1
+        end -= 1
+
 def linked_to_list(head: ListNode):
+    """
+    Convert a linked list into a list.
+    :param head:
+    :return:
+    """
     ans = [head.val]
     cur = head
     while cur.next:
@@ -10,6 +28,11 @@ def linked_to_list(head: ListNode):
 
 
 def list_to_linked(list: []):
+    """
+    Convert a list into a linked list.
+    :param list:
+    :return:
+    """
     head = ListNode(list[0])
     cur = head
     for item in list[1:]:
