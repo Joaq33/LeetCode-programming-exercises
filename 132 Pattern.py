dc1265c581f80c3e9a8884bc27cc8f7c0ad03621
Joaq33 <joakoillia@gmail.com>
2020-10-23 21:22:26 -0300

    Done

---
+++ 132 Pattern.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack, s3 = [], -float('inf')
+        for num in nums[::-1]:
+            if num < s3:
+                return True
+            while stack and stack[-1] < num:
+                s3 = stack.pop()
+            stack.append(num)
+        return False
+
+    def find132pattern2(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+        minim, maxim = first, second = nums[0], nums[1]
+        for cur in nums[2:]:
+            print(minim, cur, maxim)
+            if minim < cur < maxim:
+                return True
+            first = second
+            second = cur
+            minim = min(minim, first)
+            maxim = max(maxim, cur)
+        return False
+
+
+obj = Solution()
+
+nums = [1, 2, 3, 4, -4, -3, -5, -1]
+assert (ret := obj.find132pattern(nums)) == False, ret
+
+nums = [3, 5, 0, 3, 4]
+assert (ret := obj.find132pattern(nums)) == True, ret
+
+nums = [1, 2, 5, 4]
+assert (ret := obj.find132pattern(nums)) == True, ret
+
+nums = [3, 1, 4, 2]
+assert (ret := obj.find132pattern(nums)) == True, ret
+
+nums = [1, 2, 3, 4]
+assert (ret := obj.find132pattern(nums)) == False, ret
+
+print("Tests passed.")
