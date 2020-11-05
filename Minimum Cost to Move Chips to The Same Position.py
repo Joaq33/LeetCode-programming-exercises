663811c655e6848749008bf494392c74d7185547
Joaq33 <joakoillia@gmail.com>
2020-11-05 18:16:42 -0300

    Done

---
+++ Minimum Cost to Move Chips to The Same Position.py
@@ -0,0 +1,25 @@
+# Done
+from typing import List
+
+
+class Solution:
+    @profile
+    def minCostToMoveChips(self, position: List[int]) -> int:
+        odd = even = 0
+        for i in position:
+            if i % 2 == 0:
+                even += 1
+            else:
+                odd += 1
+        return odd if odd < even else even
+
+
+obj = Solution()
+
+position = [1, 2, 3]
+assert 1 == (ret := obj.minCostToMoveChips(position)), ret
+
+position = [2, 2, 2, 3, 3]
+assert 2 == (ret := obj.minCostToMoveChips(position)), ret
+
+print("Tests passed.")
