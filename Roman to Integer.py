42f37fc5f80aadba50b793b257934f23f7e70c65
Joaq33 <joakoillia@gmail.com>
2021-02-20 23:31:47 -0300

    Done

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,11 +1,23 @@
 # Done
 class Solution:
+    def romanToInt2(self, s: str) -> int:
+        lookup = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        ans = 0
+        previousVal = -1
+        for i in range(len(s)):
+            currentVal = lookup[s[len(s) - 1 - i]]
+            print(previousVal,currentVal)
+            if previousVal > currentVal:
+                currentVal *= -1
+            ans = ans + currentVal
+            previousVal = currentVal
+        return ans
+
     def romanToInt(self, s: str) -> int:
         val = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         ans = last = 0
         for letter in s:
             cur = val[letter]
-            # print(cur,last)
             if cur > last:
                 ans += cur - last * 2
             else:
@@ -15,6 +27,8 @@ class Solution:
 
 
 obj = Solution()
+
+assert 11 == (ret := obj.romanToInt("XI")), ret
 
 assert 9 == (ret := obj.romanToInt("IX")), ret
 
