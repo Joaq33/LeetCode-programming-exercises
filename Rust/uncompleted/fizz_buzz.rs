bed7eecf379b3fb465fc4f5fa70bc6fe3b91ee5e
Joaq33 <joakoillia@gmail.com>
2022-03-11 21:52:50 -0300

    fizz_buzz rust

--- Rust/uncompleted/fizz_buzz.rs
+++ Rust/uncompleted/fizz_buzz.rs
@@ -1,23 +1,42 @@
-///copied
+///Done
 struct Solution;
 
 impl Solution {
-    /// not optimal, just copied
     pub fn fizz_buzz(n: i32) -> Vec<String> {
-        let mut res = vec![];
-        for i in 1..=n {
-            let fizz = i % 3 == 0;
-            let buzz = i % 5 == 0;
-            let s = match (fizz, buzz) {
-                (true, true) => "FizzBuzz".to_string(),
-                (true, false) => "Fizz".to_string(),
-                (false, true) => "Buzz".to_string(),
-                (false, false) => format!("{}", i),
-            };
-            res.push(s);
+        let mut ans = Vec::new();
+        for number in 1..=n {
+            let mut phrase: String = "".to_owned();
+            if number % 3 == 0 {
+                phrase += "Fizz"
+            }
+            if number % 5 == 0 {
+                phrase += "Buzz"
+            }
+            if phrase == "" {
+                ans.push(number.to_string())
+            } else {
+                ans.push(phrase)
+            }
         }
-        res
+        ans
     }
+
+    // not optimal, just copied
+    // pub fn fizz_buzz(n: i32) -> Vec<String> {
+    //     let mut res = vec![];
+    //     for i in 1..=n {
+    //         let fizz = i % 3 == 0;
+    //         let buzz = i % 5 == 0;
+    //         let s = match (fizz, buzz) {
+    //             (true, true) => "FizzBuzz".to_string(),
+    //             (true, false) => "Fizz".to_string(),
+    //             (false, true) => "Buzz".to_string(),
+    //             (false, false) => format!("{}", i),
+    //         };
+    //         res.push(s);
+    //     }
+    //     res
+    // }
 }
 
 fn main() {
