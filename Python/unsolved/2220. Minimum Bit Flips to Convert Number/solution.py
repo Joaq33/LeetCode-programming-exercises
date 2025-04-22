907a8141e81dd821a0212114c59cce92f5c79818
Joaq33 <joakoillia@gmail.com>
2025-04-22 15:07:25 +1000

    2220 bitwise operator

--- Python/unsolved/2220. Minimum Bit Flips to Convert Number/solution.py
+++ Python/unsolved/2220. Minimum Bit Flips to Convert Number/solution.py
@@ -3,13 +3,7 @@ from icecream import ic
 
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        ic(start, str(bin(start)))
-        ic(goal, bin(goal))
-        bstart = ic(bin(start)[:1:-1])
-        bgoal = ic(bin(goal)[:1:-1])
-
-        ans = abs(len(bgoal) - len(bstart))
-        ic(ans, len(bgoal), len(bstart))
-        ans += sum([a != b for a, b in zip(bstart, bgoal)])
-        print(ans)
+        ans = 0
+        for i in range(max(start.bit_length(), goal.bit_length())):
+            ans += ((start ^ goal) >> i) & 1
         return ans
--- Python/unsolved/2220. Minimum Bit Flips to Convert Number/test_solution.py
+++ Python/unsolved/2220. Minimum Bit Flips to Convert Number/test_solution.py
@@ -1,23 +1,40 @@
 import unittest
+from pickle import FALSE
+
 from solution import Solution
 from icecream import ic
+from parameterized import parameterized
+
+ic.use_stdout()
+
+
+# null = None
+# true = True
+# false = False
+
 
 class Test(unittest.TestCase):
-    def test_1(self):
-        """expect 3"""
-        start = 10
-        goal = 7
-        expected = 3
-
-        ic(start, goal, expected)
-        print("asd?")
-        self.assertEqual(expected, Solution().minBitFlips(start=start, goal=goal))
-
-    def test_2(self):
-        """expect 3"""
-        start = 10
-        goal = 82
-        expected = 3
-
+    @parameterized.expand([
+        [
+            10,
+            7,
+            3
+        ], [
+            243,
+            640,
+            6
+        ],
+        [
+            10,
+            82,
+            3
+        ],
+        [
+            383,
+            181,
+            5]
+    ])
+    def test_(self, start, goal, expected):
+        print()  # fix pycharm output
         ic(start, goal, expected)
         self.assertEqual(expected, Solution().minBitFlips(start=start, goal=goal))
