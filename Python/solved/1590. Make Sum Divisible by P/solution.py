ffb5d77b8aa6ac9312335967d1ae683edae32718
Joaq33 <joakoillia@gmail.com>
2025-05-09 18:32:21 +1000

    .

--- Python/solved/1590. Make Sum Divisible by P/solution.py
+++ Python/solved/1590. Make Sum Divisible by P/solution.py
@@ -19,9 +19,32 @@ class Solution:
             if mod in presum:
                 res = min(res, i - presum[mod])
             presum[total % p] = i
-            ic(i,presum, total)
+            ic(i, presum, total)
 
         return res if res != len(nums) else -1
+
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        totalSum = sum(nums)
+        rem = totalSum % p
+
+        if rem == 0:
+            return 0
+
+        prefixMod = {0: -1}
+        prefixSum = 0
+        minLength = len(nums)
+
+        for i, num in enumerate(nums):
+            prefixSum += num
+            currentMod = prefixSum % p
+            targetMod = (currentMod - rem + p) % p
+
+            if targetMod in prefixMod:
+                minLength = min(minLength, i - prefixMod[targetMod])
+
+            prefixMod[currentMod] = i
+
+        return minLength if minLength < len(nums) else -1
 
     # def minSubarray(self, nums: List[int], p: int) -> int:
     #     total_sum = sum(nums)
